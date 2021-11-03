894366d1368eebfae86b8e8ca1340d5267c97f02
agolombek <91390904+agolombek@users.noreply.github.com>
2021-11-03 15:42:07 +0100

    Area_plot

--- Assignment_1/Work_In_Progress.py
+++ Assignment_1/Work_In_Progress.py
@@ -2,43 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-def mandelbrot(iter,s): # iter is the number of iterations, s is the number of samples
-  # Array for the points in the Mandelbrot set
-  stable_real = []
-  stable_imag = []
+def mandelbrot_plot(iterations,samples):
+    """
+    function returns a plot of the mandelbrot set.
+    iterations is the number of iterations each sample is checked for and
+    samples is the number of samples taken.
+    """
+    for i in range(samples): 
+        
+        # Generating c between -2 and 2
+        real_c = random.random()*4 - 2
+        imag_c = random.random()*4 - 2
+        c = real_c + imag_c*1j
+    
+        # Initialise z
+        z = 0
+    
+        for i in range(iterations):
+          norm_squared = abs(z)
+          if norm_squared > 4:
+              plt.plot(real_c, imag_c, marker='.', color='black')
+              break
+          else:
+            z = z**2+c
+        # Appending the c point in the stable set
+        plt.plot(real_c, imag_c, marker='.', color='red')
+    plt.ylim(-2,2)
+    plt.xlim(-2,2)
+    plt.show
+    
+mandelbrot_plot(100, 10000)
 
-  # Array for the points not in the Mandelbrot set
-  not_stable_real = []
-  not_stable_imag = []
-  
-  for i in range(s): 
 
-    # Generating c between -2 and 2
-    real_c = random.random()*6 - 3
-    imag_c = (random.random()*6 - 3)
-    c = real_c + imag_c*1j
 
-    # Initialise z
-    z = 0
-
-    for i in range(iter):
-      norm_squared = abs(z)
-      if norm_squared > 4:
-        not_stable_real.append(real_c)
-        not_stable_imag.append(imag_c)
-        break
-      else:
-        z = z**2+c
-    # Appending the c point in the stable set
-    stable_real.append(real_c)
-    stable_imag.append(imag_c)
-
-  return stable_real,stable_imag, not_stable_real, not_stable_imag
-
-stable_real, stable_imag, not_stable_real, not_stable_imag = mandelbrot(10000,100000)
-
-plt.plot(stable_real,stable_imag,"k.")
-plt.plot(not_stable_real,not_stable_imag,"r.")
-plt.ylim(-4,4)
-plt.xlim(-4,4)
-plt.show()
--- Assignment_1/mandelbrot.py
+++ Assignment_1/mandelbrot.py
@@ -15,33 +15,54 @@ def mandelbrot(iterations,samples): # iterations is the number of iterations, sa
   for i in range(samples): 
 
     # Generating c between -2 and 2
-    real_c = random.random()*6 - 3
-    imag_c = (random.random()*6 - 3)
+    real_c = random.uniform(-2, 0.75)
+    imag_c = random.uniform(-1.25, 1.25)
     c = real_c + imag_c*1j
 
     # Initialise z
     z = 0
 
-    for i in range(iterations):
-      norm_squared = abs(z)
-      if norm_squared > 4:
-        not_stable_real.append(real_c)
-        not_stable_imag.append(imag_c)
-        break
-      else:
-        z = z**2+c
-    # Appending the c point in the stable set
-    stable_real.append(real_c)
-    stable_imag.append(imag_c)
+    for i in range(iterations+1):
+        
+        norm_squared = abs(z)
+        if norm_squared > 4:
+            not_stable_real.append(real_c)
+            not_stable_imag.append(imag_c)
+            break
+        
+        elif i == iterations:
+            stable_real.append(real_c)
+            stable_imag.append(imag_c)
+            
+        else:
+            z = z**2+c
+    
 
   return stable_real,stable_imag, not_stable_real, not_stable_imag
 
-stable_real, stable_imag, not_stable_real, not_stable_imag = mandelbrot(100,100000000)
 
-plt.plot(stable_real,stable_imag,"k.")
-plt.plot(not_stable_real,not_stable_imag,"r.")
-plt.ylim(-4,4)
-plt.xlim(-4,4)
+"""Plot Mandelbrot set"""
+
+# stable_real, stable_imag, not_stable_real, not_stable_imag = mandelbrot(3000,1000000)
+
+# plt.plot(stable_real,stable_imag,"k.")
+# plt.plot(not_stable_real,not_stable_imag,"r.")
+# plt.xlim(-2,0.75)
+# plt.ylim(-1.25,1.25)
+# plt.show()
+
+"""Plot Area of Mandelbrot set"""
+
+sample_space = np.logspace(2, 7, 6)
+all_areas = []
+
+for samples in sample_space:
+    stable_real, stable_imag, not_stable_real, not_stable_imag = mandelbrot(2000,int(samples))
+    area = (2.75*2.5)*len(stable_real)/(len(stable_real)+len(not_stable_real))
+    all_areas.append(area)
+    print(area)
+
+plt.plot(sample_space, all_areas)
+plt.xscale('log')
 plt.show()
-
-
+    
