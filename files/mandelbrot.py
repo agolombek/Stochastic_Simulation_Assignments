59575f80ff0be409604b74806acf066ef106a727
AndreaMaestri18 <andreamaestrimaestri@gmail.com>
2021-11-03 14:03:43 +0100

    Update mandelbrot.py

--- files/mandelbrot.py
+++ files/mandelbrot.py
@@ -3,26 +3,26 @@ import matplotlib.pyplot as plt
 import random
 
 def mandelbrot(iter,s): # iter is the number of iterations, s is the number of samples
+  # Array for the points in the Mandelbrot set
+  stable_real = []
+  stable_imag = []
+
+  # Array for the points not in the Mandelbrot set
+  not_stable_real = []
+  not_stable_imag = []
+  
   for i in range(s): 
 
-    # Array for the points in the Mandelbrot set
-    stable_real = []
-    stable_imag = []
-
-    # Array for the points not in the Mandelbrot set
-    not_stable_real = []
-    not_stable_imag = []
-
     # Generating c between -2 and 2
-    real_c = random.random()*4 - 2
-    imag_c = (random.random()*4 - 2)*1j
-    c = real_c + imag_c
+    real_c = random.random()*6 - 3
+    imag_c = (random.random()*6 - 3)
+    c = real_c + imag_c*1j
 
     # Initialise z
     z = 0
 
     for i in range(iter):
-      norm_squared = z.real ** 2 + z.imag**2
+      norm_squared = abs(z)
       if norm_squared > 4:
         not_stable_real.append(real_c)
         not_stable_imag.append(imag_c)
@@ -33,13 +33,13 @@ def mandelbrot(iter,s): # iter is the number of iterations, s is the number of s
     stable_real.append(real_c)
     stable_imag.append(imag_c)
 
-  return stable_real,stable_imag
+  return stable_real,stable_imag, not_stable_real, not_stable_imag
 
-stable_real, stable_imag = mandelbrot(10,100)
-print(stable_real, stable_imag)
+stable_real, stable_imag, not_stable_real, not_stable_imag = mandelbrot(10000,100000)
 
-plt.plot(stable_real,stable_imag,"ro")
+plt.plot(stable_real,stable_imag,"k.")
+plt.plot(not_stable_real,not_stable_imag,"r.")
+plt.ylim(-4,4)
+plt.xlim(-4,4)
 plt.show()
-    
-c = 
 
