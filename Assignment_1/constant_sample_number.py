a95df5dc476c837aea46a769de65f0caf06d26fd
agolombek <91390904+agolombek@users.noreply.github.com>
2021-11-17 15:02:40 +0100

    Update constant_sample_number.py

--- Assignment_1/constant_sample_number.py
+++ Assignment_1/constant_sample_number.py
@@ -17,20 +17,41 @@ from matplotlib import cm
 from mandelbrot_functions import *
 
 ########################## CALLING AREA FUNCTION #############################
+start_time = time() 
+sample_range = np.array([100, 1000])
+x_values = np.logspace(2, 3, 5)
 
-start_time = time()  
-x_values = np.logspace(4, 5, 10)
-sample_size = 1000000
+for sample_size in sample_range:
+    
+    ##################### Randome sampling ##################################
+    title = str(sample_size) + ' Samples'
+    
+    answer = Mandelbrot_constant_samplesize(x_values, sample_size, 1e-3, random_sampling)
+    iterations = answer[:,0]
+    area = answer[:,1] 
+    error = area - 1.506484193
+    plt.plot(iterations, error, label='Random sampling') 
+    
+    #################### Hypercube sampling ##################################
+    title = str(sample_size) + 'samples'
         
-answer = Mandelbrot_constant_samplesize(x_values, sample_size, 1e-3, random_sampling)
-
-iterations = answer[:,0]
-area = answer[:,2]
-bootstrap_it = answer[:,3]
-         
+    answer = Mandelbrot_constant_samplesize(x_values, sample_size, 1e-3, latin_hypercube_sampling)
+    iterations = answer[:,0]
+    area = answer[:,1]
+    error = area - 1.506484193
+    plt.plot(iterations, error, label='Hypercube sampling')
+    
+    # Finish Plot
+    plt.grid()
+    plt.xscale('log')
+    plt.xlabel('Iterations', fontsize=18)
+    plt.ylabel('Error', fontsize=18)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.legend(fontsize=18)
+    plt.title(title, fontsize=22)
+    plt.savefig(title)
+    
 end_time = time()
 
-print('The runtime was', (end_time-start_time)/(60*60), 'hours')   
-
-############################# PLOTTING ######################################
-
+print('The runtime was', (end_time-start_time)/(60*60), 'hours')  
