a77a6f14aef0729173e2c783cbd57ab67ac0ac02
AndreaMaestri18 <andreamaestrimaestri@gmail.com>
2021-11-17 22:33:38 +0100

    orthogonal sampling

--- Assignment_1/orthogonal.py
+++ Assignment_1/orthogonal.py
@@ -8,45 +8,55 @@ from numba import njit
 
 
 @njit
-def orthogonal(major):
-	samples = major * major
-	vec_x = np.zeros((samples))
-	vec_y = np.zeros((samples))
+def orthogonal_array(major):
+	array1 = np.zeros((major*major))
+	array2 = np.zeros((major*major))
 
-	xlist = np.zeros((major,major))
-	ylist = np.zeros((major,major))
-	samples_size = major*major
-	m = 0
-	for i in range(major):
-
-		for j in range(major):
-
-			m += 1
-			xlist[i][j] = ylist[i][j] = m
-
-	for k in range(1):
-
+	# changing the elements of the second column
+	for j in range(major):
 		for i in range(major):
-			xlist[i] = np.random.permutation(xlist[i])
-			ylist[i] = np.random.permutation(ylist[i])
+			array2[i+major*j]=i+1
 
-	for i in range(major):
+	return array1, array2
 
+
+def orthogonal_sampling(major):
+	
+	samples_vector = np.zeros((major*major,2))
+	col1 = np.array([])
+	col2 = np.zeros((major*major))
+	#col1,col2 = orthogonal_array(major)
+	# devo cambiare gli elementi a gruppi di #major
+	for i in range(1,major+1):
+		col1 = np.append(col1,np.random.permutation([(i-1)*major+j for j in range(1,major+1)]))
+
+	# changing the elements of the second column
+	for j in range(major):
+		for i in range(major):
+			col2[i+major*j]=i+1
+	for i in range(1,major+1):
+		change = np.random.permutation([(i-1)*major+j for j in range(1,major+1)])
 		for j in range(major):
-			scale = 1
-			x = -2.0 + scale * xlist[i][j] + np.random.random()
-			y = -2.0 + scale * ylist[i][j] + np.random.random()	
-			vec_x[i*j] = x
-			vec_y[i*j] = y
-	return vec_x,vec_y
+			col2[i-1+j*major] = change[j]
+	for i in range(major*major):
+		samples_vector[i,0] = ((np.random.random()+col1[i]-1)/major**2)*2.75-2
+		samples_vector[i,1] = ((np.random.random()+col2[i]-1)/major**2)*2.5-1.25
 
-x,y = orthogonal(10)
+	return samples_vector
 
-for i in range(100):
+		
+all_samples = orthogonal_sampling(8)
 
-	for j in range(100):
-
-		plt.plot(x[i],y[j],"r.")
+for i in np.linspace(-2,0.75,9):
+    for j in np.linspace(-1.25,1.25,9):
+        plt.plot(i,j,"b.")
+for point in all_samples:
+    plt.plot(point[0], point[1],"r.")
 
 plt.show()
+
+
+
+
+
 
