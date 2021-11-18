c909e2a53abddf45e5efc6e6239d89b423d125cb
AndreaMaestri18 <andreamaestrimaestri@gmail.com>
2021-11-18 14:01:49 +0100

    up

--- Assignment_1/src/constant_sample_number.py
+++ Assignment_1/src/constant_sample_number.py
@@ -19,31 +19,33 @@ from mandelbrot_functions import *
 ########################## CALLING AREA FUNCTION #############################
 start_time = time() 
 # using the sqrt because we need to use that for the orthogonal sampling
+
 sample_range_sqrt = np.logspace(0.5,1,2)
 x_values = np.logspace(1, 2, 20)
 
-for sample_size_sqrt in sample_range_sqrt:
 
+for sample_size_sqrt in [10,100,300,500]:
+
+    print(orthogonal_sampling(int(sample_size_sqrt)))
     sample_size = int(sample_size_sqrt ** 2) 
-    print(sample_size, sample_size_sqrt)
     fig, ax = plt.subplots()
     
     ##################### Randome sampling ##################################
     title = str(int(sample_size)) + ' Samples'
     
-    answer = Mandelbrot_constant_samplesize(x_values, sample_size, 1e-3, random_sampling)
-    iterations = answer[:,0]
-    area = answer[:,1] 
-    error = abs(area - 1.506484193)
-    ax.plot(iterations, error, label='Random sampling') 
+    # answer = Mandelbrot_constant_samplesize(x_values, sample_size, 1e-3, random_sampling)
+    # iterations = answer[:,0]
+    # area = answer[:,1] 
+    # error = abs(area - 1.506484193)
+    # ax.plot(iterations, error, label='Random sampling') 
     
     # #################### Hypercube sampling ##################################
         
-    answer = Mandelbrot_constant_samplesize(x_values, sample_size, 1e-3, latin_hypercube_sampling)
-    iterations = answer[:,0]
-    area = answer[:,1]
-    error = abs(area - 1.506484193)
-    ax.plot(iterations, error, label='Hypercube sampling')
+    # answer = Mandelbrot_constant_samplesize(x_values, sample_size, 1e-3, latin_hypercube_sampling)
+    # iterations = answer[:,0]
+    # area = answer[:,1]
+    # error = abs(area - 1.506484193)
+    # ax.plot(iterations, error, label='Hypercube sampling')
 
     #################### Orthogonal sampling ##################################
 
@@ -53,14 +55,14 @@ for sample_size_sqrt in sample_range_sqrt:
     error = abs(area - 1.506484193)
     ax.plot(iterations, error, label='Orthogonal sampling')
 
-    # Finish Plot
-    ax.grid(axis='both')
-    ax.set_xscale('log')
-    ax.set_xlabel('Iterations')
-    ax.set_ylabel('Error')
-    ax.legend()
-    ax.set_title(title)
-    plt.show()
+    # # Finish Plot
+    # ax.grid(axis='both')
+    # ax.set_xscale('log')
+    # ax.set_xlabel('Iterations')
+    # ax.set_ylabel('Error')
+    # ax.legend()
+    # ax.set_title(title)
+    # plt.show()
     
 end_time = time()
 
