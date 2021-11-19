baa5798482438f5370c9d682ebfc366c3866937c
agolombek <91390904+agolombek@users.noreply.github.com>
2021-11-19 14:02:20 +0100

    more functions

--- Assignment_1/src/WIP2.py
+++ Assignment_1/src/WIP2.py
@@ -16,68 +16,14 @@ from matplotlib.ticker import MaxNLocator
 from matplotlib import cm
 from mandelbrot_functions import *
 
-@njit
-def function_convergence(all_iterations, sqrt_sample_size, max_std):
-    answer = np.zeros((all_iterations.size, 4))
-    grid_point = 0
-    for iteration in all_iterations:
-        sample_size = int(sqrt_sample_size**2)
-        random_sample = random_sampling(int(sqrt_sample_size))
-        hypercube_sample = latin_hypercube_sampling(int(sqrt_sample_size))
-        ortho_sample = orthogonal_sampling(int(sqrt_sample_size))
-        
-        Area_random, bootstrap_itr1 = Mandelbrot_Area(int(iteration), random_sample, max_std)
-        Area_hypercube, bootstrap_itr2 = Mandelbrot_Area(int(iteration), hypercube_sample, max_std)
-        Area_ortho, bootstrap_itr3 = Mandelbrot_Area(int(iteration), ortho_sample, max_std)
-        
-        
-        answer[grid_point,0] = int(iteration)
-        answer[grid_point,1] = Area_random
-        answer[grid_point,2] = Area_hypercube
-        answer[grid_point,3] = Area_ortho
-        grid_point += 1
-    
-    
-    answer[:,1] = np.absolute(answer[:,1] - answer[-1,1])
-    answer[:,2] = np.absolute(answer[:,2] - answer[-1,2])
-    answer[:,3] = np.absolute(answer[:,3] - answer[-1,3])
-    return answer
-
-@njit
-def real_value_convergence(all_iterations, sqrt_sample_size, max_std):
-    answer = np.zeros((all_iterations.size, 4))
-    grid_point = 0
-    for iteration in all_iterations:
-        sample_size = int(sqrt_sample_size**2)
-        random_sample = random_sampling(int(sqrt_sample_size))
-        hypercube_sample = latin_hypercube_sampling(int(sqrt_sample_size))
-        ortho_sample = orthogonal_sampling(int(sqrt_sample_size))
-        
-        Area_random, bootstrap_itr1 = Mandelbrot_Area(int(iteration), random_sample, max_std)
-        Area_hypercube, bootstrap_itr2 = Mandelbrot_Area(int(iteration), hypercube_sample, max_std)
-        Area_ortho, bootstrap_itr3 = Mandelbrot_Area(int(iteration), ortho_sample, max_std)
-        
-        
-        answer[grid_point,0] = int(iteration)
-        answer[grid_point,1] = Area_random
-        answer[grid_point,2] = Area_hypercube
-        answer[grid_point,3] = Area_ortho
-        grid_point += 1
-    
-    real_area = 1.506484193
-    answer[:,1] = np.absolute(answer[:,1] - real_area)
-    answer[:,2] = np.absolute(answer[:,2] - real_area)
-    answer[:,3] = np.absolute(answer[:,3] - real_area)
-    return answer
-
 
 start_time = time() 
 
 all_iterations = np.logspace(2, 4, 30)
 sqrt_sample_size = 100
-max_std = 1e-3
+max_std = 1e-4
 
-answer = function_convergence(all_iterations, sqrt_sample_size, max_std)
+answer = real_value_convergence(all_iterations, sqrt_sample_size, max_std)
 
 ITERATIONS= answer[:,0]
 ERROR_RANDOM= answer[:,1]
